48eefc9b659c0b87e14b6d32c6fdb560486248ce
hj-keem <hyeomjeom@gmail.com>
2023-11-08 17:36:00 +0900

    first push

---
+++ src/main/java/com/example/usedmarket/UsedmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.usedmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedmarketApplication.class, args);
+	}
+
+}
