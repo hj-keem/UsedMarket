e2e5e08b816b88eac3fda16824611d499b87b64e
hj-keem <hyeomjeom@gmail.com>
2024-08-16 22:52:36 +0900

    권한 Repo 추가

---
+++ src/main/java/com/example/usedmarket/repo/AuthorityRepo.java
@@ -0,0 +1,7 @@
+package com.example.usedmarket.repo;
+
+import com.example.usedmarket.entity.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepo extends JpaRepository<Authority, Long> {
+}
