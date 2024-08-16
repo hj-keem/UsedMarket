02746f76f1dc59a9491f4dd0c4ad9d96725fa4f2
hj-keem <hyeomjeom@gmail.com>
2024-08-16 22:50:52 +0900

    findByItemId 추가

--- src/main/java/com/example/usedmarket/repo/NegoRepo.java
+++ src/main/java/com/example/usedmarket/repo/NegoRepo.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NegoRepo extends JpaRepository<NegoEntity, Long> {
-    List<NegoEntity> findBySalesItemId(Long itemId);
+    List<NegoEntity> findByItemId(Long itemId);
 }
